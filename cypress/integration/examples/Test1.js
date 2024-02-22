88b8d258f56e4b5b059e9eef3a95d1a3a6f1a2ff
Dannyy2000 <akinsanyadaniel665@gmail.com>
2024-02-22 15:39:19 +0100

    loginTest

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,14 @@
+describe("The organization admin signIn",function(){
+
+    it("signIn",function(){
+ 
+      cy.visit("https://systest-app.enum.africa/login")
+      cy.get('input[name="email"]').type('salah@mailinator.com')
+      cy.get('input[name="password"]').type('Password321$')
+ 
+      cy.get('button[type="submit"]').click()
+ 
+      cy.url().should('include','https://systest-app.enum.africa/workspace');
+ 
+    })
+  })
