66d6bb62664522005277d7164aae91c7072114e7
Dannyy2000 <akinsanyadaniel665@gmail.com>
2024-02-22 20:04:59 +0100

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/integration/examples/*.js'
+    specPattern: 'cypress/integration/examples/*.js',
+    projectId: '8c79dc07-5fe7-4fa5-a1d6-6c6d8ebdbf6a',
   },
 });
